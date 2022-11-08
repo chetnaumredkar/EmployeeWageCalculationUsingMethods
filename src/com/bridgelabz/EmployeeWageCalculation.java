6a1f4b39c48fd84bfbc07f79ac853f4766ca8f93
kolhechetna <kolhe.chetna@gmail.com>
2022-11-08 18:36:00 +0530

    Added:Attendence Employee Present or Not

--- src/com/bridgelabz/EmployeeWageCalculation.java
+++ src/com/bridgelabz/EmployeeWageCalculation.java
@@ -1,10 +1,28 @@
 package com.bridgelabz;
 
 public class EmployeeWageCalculation {
+	static int IS_FULL_TIME = 1;
+	static int PART_TIME = 0;
+	double empCheck;
+	int empHrs;
+	int empWage = 1;
+	static int Emp_Rate_Per_Hour = 20;
+
+	public void attendence() {
+		empCheck = Math.floor(Math.random() * 2);
+		System.out.println(empCheck);
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is not Present");
+		}
+
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee\r\n"
-				+ "Wage Computation\r\n"
-				+ "Program on Master Branch");
+		System.out.println("Welcome to Employee\r\n" + "Wage Computation\r\n" + "Program on Master Branch");
+		EmployeeWageCalculation obj = new EmployeeWageCalculation();
+		obj.attendence();
 	}
 
 }
