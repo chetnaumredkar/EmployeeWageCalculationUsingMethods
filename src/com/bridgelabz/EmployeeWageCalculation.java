e91facab9fcba497c2c06ffa8975cb182823c9f6
kolhechetna <kolhe.chetna@gmail.com>
2022-11-08 18:46:57 +0530

    Added:Also added Half day Wage calculation as well

--- src/com/bridgelabz/EmployeeWageCalculation.java
+++ src/com/bridgelabz/EmployeeWageCalculation.java
@@ -2,27 +2,34 @@ package com.bridgelabz;
 
 public class EmployeeWageCalculation {
 	static int IS_FULL_TIME = 1;
-	static int PART_TIME = 0;
+	static int PART_TIME =2 ;
+	static int ABSENT=0;
 	double empCheck;
 	int empHrs;
 	int empWage = 1;
 	static int Emp_Rate_Per_Hour = 20;
 
 	public void attendence() {
-		empCheck = Math.floor(Math.random() * 2);
+		empCheck = Math.floor(Math.random() * 3);
 		System.out.println(empCheck);
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present");
-		} else {
+		} else if(empCheck == ABSENT) {
 			System.out.println("Employee is not Present");
+		}
+			else {
+				System.out.println("Employee Present For Half Day");
 		}
 	}
 
 	public void daily_Employee_Wage() {
-		if (empCheck == IS_FULL_TIME) {
+		if (empCheck == PART_TIME) {
 			empHrs = 8;
-		} else {
-			empHrs = 0;
+		} else if(empCheck == IS_FULL_TIME){
+			empHrs =16;
+		}
+		else {
+			empHrs=0;
 		}
 		empWage = empHrs * Emp_Rate_Per_Hour;
 		System.out.println("emp wage: " + empWage);
