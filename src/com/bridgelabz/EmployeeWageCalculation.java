5fa84a1c1d752c993417399521537f5e60330c41
kolhechetna <kolhe.chetna@gmail.com>
2022-11-08 17:53:45 +0530

    Added:Display Message

---
+++ src/com/bridgelabz/EmployeeWageCalculation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class EmployeeWageCalculation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee\r\n"
+				+ "Wage Computation\r\n"
+				+ "Program on Master Branch");
+	}
+
+}
